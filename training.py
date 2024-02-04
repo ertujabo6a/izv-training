194b84005b65f9a0f12a166c3c2b3a93ac450357
AalleexxFfrr <dmitrievaleksandr2001@gmail.com>
2024-02-04 04:04:55 +0100

    code: add generate_graph function and pic of the result

--- training.py
+++ training.py
@@ -26,3 +26,39 @@ def integrate(f: Callable[[np.ndarray], np.ndarray], a: float, b: float, steps=1
     integral_value = np.sum(y_values) * dx
 
     return integral_value
+
+def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None = None):
+    # 'a' values fo calculation
+    a_values = np.array([1.0, 1.5, 2.0])
+
+    # Range of x
+    x = np.linspace(-3, 3, 1000)
+
+    # Calculate values of f(a, x) for all combinations of 'a' and 'x'
+    X, A = np.meshgrid(x, a_values)
+    result_matrix = A**2 * X**3 * np.sin(X)
+
+    # Calculate integrals for each row of the matrix
+    integrals = np.trapz(result_matrix, x, axis=1)
+
+    # Create the graph
+    fig, ax = plt.subplots(figsize=(10, 6))
+    for i, a in enumerate(a_values):
+        ax.plot(x, result_matrix[i], label=f'$γ_{a}(x)$')
+        ax.fill_between(x, result_matrix[i], alpha=0.1)
+        
+        # Add text to the right of the end of the each graph
+        ax.annotate(f'$\\int f_{a:}(x)dx= {integrals[i]:.2f}$', xy=(x[-1], result_matrix[i][-1]))
+
+    # Set axis labels, legend, and graph title
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$f_a(x)$')
+    ax.grid(True)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=3)
+    plt.tight_layout()
+
+    if save_path:
+        plt.savefig(save_path)
+    
+    if show_figure:
+        plt.show()
