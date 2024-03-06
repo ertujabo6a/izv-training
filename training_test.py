c23221a8494a2da6e01330c71d55665dfc6d301d
Aleksandr Dmitriev <dmitrievaleksandr2001@gmail.com>
2024-03-06 01:15:10 +0100

    refactor: update test

--- training_test.py
+++ training_test.py
@@ -7,7 +7,7 @@ Execution:
 or
    python3 -m pytest
 """
-import part01
+import training
 import numpy as np
 import os
 import pytest
@@ -16,26 +16,26 @@ import pytest
 def test_integrate():
     """Test calculation of the integral"""
     def f(x): return 10 * x + 2
-    r = part01.integrate(f, 0, 1, 100)
+    r = training.integrate(f, 0, 1, 100)
     assert r == pytest.approx(7)
 
 
 def test_generate_fn():
     "Test generating graphs with multiple functions"
-    part01.generate_graph([1., 2., -2.], show_figure=False,
+    training.generate_graph([1., 2., -2.], show_figure=False,
                           save_path="tmp_fn.png")
     assert os.path.exists("tmp_fn.png")
 
 
 def test_generate_sin():
     "Test generating graphs with sine functions"
-    part01.generate_sinus(show_figure=False, save_path="tmp_sin.png")
+    training.generate_sinus(show_figure=False, save_path="tmp_sin.png")
     assert os.path.exists("tmp_sin.png")
 
 
 def test_download():
     "Test data download"
-    data = part01.download_data()
+    data = training.download_data()
 
     assert len(data) == 40
     assert data[0]["position"] == "Cheb"
